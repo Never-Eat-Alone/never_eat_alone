bf6a11aeaadf1ad14f1ec8d2b8c981db16c61b6a
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2022-05-12 17:18:45 -0400

    Adding the text button component.

---
+++ client/library/source/components/text_button.tsx
@@ -0,0 +1,110 @@
+import { css, StyleSheet } from 'aphrodite';
+import * as React from 'react';
+
+interface Properties {
+  label: string;
+  backgroundColor: string;
+  width: string;
+  height: string;
+  border: string;
+  className: React.CSSProperties;
+  isDisabled?: boolean;
+  style?: React.CSSProperties;
+  onClick: () => void;
+}
+
+export class TextButton extends React.Component<Properties> {
+  public render(): JSX.Element {
+    return (
+      <button
+          style={{...BUTTON_STYLE,
+            backgroundColor: this.props.backgroundColor,
+            width: this.props.width,
+            height: this.props.height,
+            border: this.props.border,
+            ...this.props.style
+          }}
+          className={css(this.props.className)}
+          disabled={this.props.isDisabled}
+          onClick={this.props.onClick}
+      >
+        <p style={LABEL_STYLE} >{this.props.label}</p>
+      </button>);
+  }
+}
+
+interface TextButtonInterface {
+  label: string;
+  isDisabled?: boolean;
+  style?: React.CSSProperties;
+  onClick: () => void;
+}
+
+export function InvertedSecondaryTextButton(props: TextButtonInterface) {
+  return (<TextButton
+    backgroundColor='transparent'
+    width='68px'
+    height='29px'
+    border='1px solid #FFFFFF'
+    className={styles.invertedSecondaryTextButton}
+    {...props}
+  />);
+}
+
+const BUTTON_STYLE: React.CSSProperties = {
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '4px',
+  outline: 'none',
+  margin: '0px',
+  padding: '0px',
+  cursor: 'pointer',
+  overflow: 'hidden',
+  boxShadow: 'none'
+};
+
+const LABEL_STYLE: React.CSSProperties = {
+  fontFamily: 'Source Sans Pro',
+  fontStyle: 'normal',
+  fontWeight: 600,
+  fontSize: '10px',
+  lineHeight: '13px',
+  height: '13px',
+  textTransform: 'uppercase',
+  color: '#FFFFFF'
+};
+
+const styles = StyleSheet.create({
+  invertedSecondaryTextButton: {
+    ':hover': {
+      backgroundColor: '#F26B55',
+      boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.4)',
+      color: '#FFFFFF'
+    },
+    ':focus': {
+      backgroundColor: '#F26B55',
+      boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.4)',
+      color: '#FFFFFF'
+    },
+    ':focus-within': {
+      backgroundColor: '#F26B55',
+      boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.4)',
+      color: '#FFFFFF'
+    },
+    ':active': {
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+      border: '2px solid #FFFFFF',
+      color: '#FFFFFF'
+    },
+    ':disabled': {
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+      border: '1px solid #F6F6F6',
+      color: '#F6F6F6'
+    }
+  }
+});
--- client/tests/ui_catalog/source/data/component_schemas.ts
+++ client/tests/ui_catalog/source/data/component_schemas.ts
@@ -4,8 +4,14 @@ import { ComponentSchema, PropertySchema } from './schemas';
 
 /** Loads the complete list of schemas available to test. */
 export function loadComponentSchemas(): ComponentSchema[] {
-  const logoSchema = new ComponentSchema('HeaderLogoWithWhiteText',
+  const logoSchema = new ComponentSchema('WhiteTextHeaderLogo',
     [new PropertySchema('style', {}, CSSInput)], [],
-    NeverEatAlone.HeaderLogoWithWhiteText);
+    NeverEatAlone.WhiteTextHeaderLogo);
+  const InvertedSecondaryTextButton = new ComponentSchema(
+    'InvertedSecondaryTextButton', [new PropertySchema('style', {}, CSSInput),
+    new PropertySchema('label', 'LOGIN', String),
+    new PropertySchema('isDisabled', false, Boolean),
+    new PropertySchema('onClick', null, () => void)],
+    [], NeverEatAlone.InvertedSecondaryTextButton);
   return [logoSchema];
 }
