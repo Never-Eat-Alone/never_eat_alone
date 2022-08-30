d1c4d64301522c3d7d779b47ad2ba2f279647fc1
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2022-08-30 17:02:10 -0400

    Reordered the component schema list alphabetically. Add the join event modal to the components.

--- client/library/source/modals/index.ts
+++ client/library/source/modals/index.ts
@@ -1,3 +1,4 @@
+export * from './join_event';
 export * from './join_modal';
 export * from './join_request_sent_modal';
 export * from './log_in_modal';
---
+++ client/library/source/modals/join_event.tsx
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import { CloseButton, PrimaryTextButton } from '../components';
+import { DisplayMode } from '../definitions';
+
+interface Properties {
+  displayMode: DisplayMode;
+
+  eventFee: number;
+
+  eventTitle: string;
+
+  eventStartDate: Date;
+
+  onJoinEvent: () => void;
+  onClose: () => void;
+}
+
+/** Displays the Join Event Modal. */
+export class JoinEventModal extends React.Component<Properties> {
+  public render(): JSX.Element {
+    const { containerStyle, formContainerStyle } = (() => {
+      if (this.props.displayMode === DisplayMode.DESKTOP) {
+        return {
+          containerStyle: DESKTOP_CONTAINER_STYLE,
+          formContainerStyle: DESKTOP_FORM_CONTAINER_STYLE
+        };
+      }
+      if (this.props.displayMode === DisplayMode.TABLET) {
+        return {
+          containerStyle: TABLET_CONTAINER_STYLE,
+          formContainerStyle: TABLET_FORM_CONTAINER_STYLE
+        };
+      }
+      return {
+        containerStyle: MOBILE_CONTAINER_STYLE,
+        formContainerStyle: MOBILE_FORM_CONTAINER_STYLE
+      };
+    })();
+    return (
+      <div style={containerStyle} >
+        <CloseButton
+          style={CLOSE_BUTTON_STYLE}
+          displayMode={this.props.displayMode}
+          onClick={this.props.onClose}
+        />
+        <div style={formContainerStyle} >
+          
+          <PrimaryTextButton
+            style={JOIN_BUTTON_STYLE}
+            label='Join Event'
+            onClick={this.props.onJoinEvent}
+          />
+        </div>
+      </div>);
+  }
+}
+
+const CONTAINER_STYLE: React.CSSProperties = {
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  position: 'relative',
+  height: '486px',
+  width: '687px',
+  boxShadow: '0px 1px 4px rgba(86, 70, 40, 0.25)',
+  borderRadius: '4px',
+  overflow: 'hidden'
+};
+
+const DESKTOP_CONTAINER_STYLE: React.CSSProperties = {
+
+};
+
+const TABLET_CONTAINER_STYLE: React.CSSProperties = {
+
+};
+
+const MOBILE_CONTAINER_STYLE: React.CSSProperties = {
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  position: 'relative',
+  width: '335px',
+  height: '482px',
+  boxShadow: '0px 1px 4px rgba(86, 70, 40, 0.25)',
+  borderRadius: '4px',
+  overflow: 'hidden'
+};
+
+const CLOSE_BUTTON_STYLE: React.CSSProperties = {
+  position: 'absolute',
+  top: '20px',
+  right: '20px'
+};
+
+const DESKTOP_FORM_CONTAINER_STYLE: React.CSSProperties = {
+
+};
+
+const TABLET_FORM_CONTAINER_STYLE: React.CSSProperties = {
+  
+};
+
+const MOBILE_FORM_CONTAINER_STYLE: React.CSSProperties = {
+  
+};
+
+const JOIN_BUTTON_STYLE: React.CSSProperties = {
+
+};
