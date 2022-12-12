0b5c71af4309a48e8cc826f86c86071e4872da4e
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2022-12-12 12:15:29 -0500

    Added the feedback submitted and survey.

--- client/library/source/pages/deleted_account_survey_page/deleted_account_survey_page.tsx
+++ client/library/source/pages/deleted_account_survey_page/deleted_account_survey_page.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
-import { CheckBox, PrimaryTextButton, Textarea, RedNavLink } from '../../components';
+import { CheckBox, PrimaryTextButton, Textarea, RedNavLink
+} from '../../components';
 import { DisplayMode } from '../../definitions';
 
-const SurveyOptions = {
-  o1: "I don’t find it useful.",
-  o2: "I don’t understand how it works.",
-  o3: 'There are no events that interest me.',
-  o4: 'There are no events in my area.',
-  o5: 'This is a temporary break / I want to make a new account.',
-  o6: 'I have a privacy or safety concern.'
-};
+const SurveyOptions = ["I don’t find it useful.",
+  "I don’t understand how it works.", 'There are no events that interest me.',
+  'There are no events in my area.',
+  'This is a temporary break / I want to make a new account.',
+  'I have a privacy or safety concern.'
+];
 
 export class SurveyAnswers {
   constructor(a1: boolean, a2: boolean, a3: boolean, a4: boolean, a5: boolean,
@@ -67,7 +66,13 @@ interface Properties {
 }
 
 interface State {
-  surveyAnswers: SurveyAnswers;
+  message: string;
+  a1: boolean;
+  a2: boolean;
+  a3: boolean;
+  a4: boolean;
+  a5: boolean;
+  a6: boolean;
 }
 
 export class DeletedAccountSurveyPage extends React.Component<Properties, State
@@ -75,20 +80,26 @@ export class DeletedAccountSurveyPage extends React.Component<Properties, State
   constructor(props: Properties) {
     super(props);
     this.state = {
-      surveyAnswers: new SurveyAnswers(false, false, false, false, false, false,
-        '')
+      message: '',
+      a1: false,
+      a2: false,
+      a3: false,
+      a4: false,
+      a5: false,
+      a6: false
     };
   }
 
   public render(): JSX.Element {
-    const { containerStyle, contentStyle, imageContainerStyle, imageStyle } = (
-        () => {
+    const { containerStyle, contentStyle, imageContainerStyle, imageStyle,
+        surveyContainerStyle } = (() => {
       if (this.props.displayMode === DisplayMode.DESKTOP) {
         return {
           containerStyle: DESKTOP_CONTAINER_STYLE,
           contentStyle: DESKTOP_CONTENT_STYLE,
           imageContainerStyle: DESKTOP_IMAGE_CONTAINER_STYLE,
-          imageStyle: DESKTOP_IMAGE_STYLE
+          imageStyle: DESKTOP_IMAGE_STYLE,
+          surveyContainerStyle: DESKTOP_SURVEY_CONTAINER_STYLE
         };
       }
       if (this.props.displayMode === DisplayMode.TABLET) {
@@ -96,26 +107,97 @@ export class DeletedAccountSurveyPage extends React.Component<Properties, State
           containerStyle: TABLET_CONTAINER_STYLE,
           contentStyle: TABLET_CONTENT_STYLE,
           imageContainerStyle: TABLET_IMAGE_CONTAINER_STYLE,
-          imageStyle: TABLET_IMAGE_STYLE
+          imageStyle: TABLET_IMAGE_STYLE,
+          surveyContainerStyle: TABLET_SURVEY_CONTAINER_STYLE
         };
       }
       return {
         containerStyle: MOBILE_CONTAINER_STYLE,
         contentStyle: MOBILE_CONTENT_STYLE,
         imageContainerStyle: MOBILE_IMAGE_CONTAINER_STYLE,
-        imageStyle: MOBILE_IMAGE_STYLE
+        imageStyle: MOBILE_IMAGE_STYLE,
+        surveyContainerStyle: MOBILE_SURVEY_CONTAINER_STYLE
       };
     })();
     const surveyContentSection = (() => {
       if (this.props.isSubmitted) {
         return (
-          <div>
-
+          <div style={THANKS_CONTAINER_STYLE} >
+            <img
+              style={HEART_IMAGE_STYLE}
+              src='resources/icons/appreciation.svg'
+              alt='Appreciation Icon'
+            />
+            <h2 style={H2_TEXT_STYLE} >Thanks for your feedback!</h2>
           </div>);
       }
+      const surveyRows = [];
+      surveyRows.push(
+        <div key='option-1' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[0]}
+            checked={this.state.a1}
+            onBoxClick={this.handleCheckBoxA1Click}
+          />
+        </div>);
+      surveyRows.push(
+        <div key='option-2' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[1]}
+            checked={this.state.a2}
+            onBoxClick={this.handleCheckBoxA2Click}
+          />
+        </div>);
+      surveyRows.push(
+        <div key='option-3' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[2]}
+            checked={this.state.a3}
+            onBoxClick={this.handleCheckBoxA3Click}
+          />
+        </div>);
+      surveyRows.push(
+        <div key='option-4' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[3]}
+            checked={this.state.a4}
+            onBoxClick={this.handleCheckBoxA4Click}
+          />
+        </div>);
+      surveyRows.push(
+        <div key='option-5' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[4]}
+            checked={this.state.a5}
+            onBoxClick={this.handleCheckBoxA5Click}
+          />
+        </div>);
+      surveyRows.push(
+        <div key='option-6' style={SURVEY_ROW_STYLE} >
+          <CheckBox
+            label={SurveyOptions[5]}
+            checked={this.state.a6}
+            onBoxClick={this.handleCheckBoxA6Click}
+          />
+        </div>);
       return (
-        <div>
-
+        <div style={surveyContainerStyle} >
+          <p style={TEXT_STYLE} >
+            Take a few moments to let us know why you’re leaving?
+          </p>
+          {surveyRows}
+          <Textarea
+            style={TEXT_AREA_STYLE}
+            placeholder='Share any other thoughts or feedback to help us 
+              improve our app!'
+            value={this.state.message}
+            onValueChange={this.handleTextAreaValueChange}
+          />
+          <PrimaryTextButton
+            style={SUBMIT_BUTTON_STYLE}
+            label='Submit'
+            onClick={this.handleSubmit}
+          />
         </div>);
     })();
     return (
@@ -137,6 +219,41 @@ export class DeletedAccountSurveyPage extends React.Component<Properties, State
           {surveyContentSection}
         </div>
       </div>);
+  }
+
+  private handleTextAreaValueChange = (newMessage: string) => {
+    this.setState({ message: newMessage });
+  }
+
+  private handleCheckBoxA1Click = () => {
+    this.setState((prevState) => ({ a1: !prevState.a1 }));
+  }
+
+  private handleCheckBoxA2Click = () => {
+    this.setState((prevState) => ({ a2: !prevState.a2 }));
+  }
+
+  private handleCheckBoxA3Click = () => {
+    this.setState((prevState) => ({ a3: !prevState.a3 }));
+  }
+
+  private handleCheckBoxA4Click = () => {
+    this.setState((prevState) => ({ a4: !prevState.a4 }));
+  }
+
+  private handleCheckBoxA5Click = () => {
+    this.setState((prevState) => ({ a5: !prevState.a5 }));
+  }
+
+  private handleCheckBoxA6Click = () => {
+    this.setState((prevState) => ({ a6: !prevState.a6 }));
+  }
+
+  private handleSubmit = () => {
+    const surveyAnswers = new SurveyAnswers(this.state.a1, this.state.a2,
+      this.state.a3, this.state.a4, this.state.a5, this.state.a6,
+      this.state.message);
+    this.props.onSubmit(surveyAnswers);
   }
 }
 
@@ -247,13 +364,94 @@ const TEXT_STYLE: React.CSSProperties = {
   fontWeight: 400,
   fontSize: '18px',
   lineHeight: '23px',
-  color: '#000000'
+  color: '#000000',
+  padding: '0px',
+  margin: '0px'
 };
 
 const LINK_STYLE: React.CSSProperties = {
   fontWeight: 400,
   fontSize: '18px',
   lineHeight: '23px',
-  minHeight: '18px',
+  minHeight: '23px',
+  height: 'auto',
   width: 'fit-content'
+};
+
+const DESKTOP_SURVEY_CONTAINER_STYLE: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  gap: '20px',
+  width: '456px'
+}
+
+const TABLET_SURVEY_CONTAINER_STYLE: React.CSSProperties = {
+  ...DESKTOP_SURVEY_CONTAINER_STYLE
+};
+
+const MOBILE_SURVEY_CONTAINER_STYLE: React.CSSProperties = {
+  ...DESKTOP_SURVEY_CONTAINER_STYLE,
+  width: '100%'
+};
+
+const SURVEY_ROW_STYLE: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  width: '100%',
+  height: '18px'
+};
+
+const TEXT_AREA_STYLE: React.CSSProperties = {
+  width: '100%',
+  fontSize: '18px',
+  lineHeight: '23px',
+  whiteSpace: 'pre-line'
+};
+
+const SUBMIT_BUTTON_STYLE: React.CSSProperties = {
+  width: '113px',
+  minWidth: '113px',
+  height: '35px',
+  minHeight: '35px',
+  fontWeight: 600,
+  fontSize: '12px',
+  lineHeight: '15px'
+};
+
+const THANKS_CONTAINER_STYLE: React.CSSProperties = {
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  paddingTop: '50px',
+  gap: '15px',
+  height: '100px',
+  width: '100%'
+};
+
+const HEART_IMAGE_STYLE: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '50px',
+  height: '50px',
+  minWidth: '50px',
+  minHeight: '50px'
+};
+
+const H2_TEXT_STYLE: React.CSSProperties = {
+  padding: '0px',
+  margin: '0px',
+  fontFamily: 'Oswald',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: '24px',
+  lineHeight: '36px',
+  color: '#000000'
 };
