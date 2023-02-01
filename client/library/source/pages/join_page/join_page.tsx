3513e61a4db75a1c376a7d05301d51dd438b8ae6
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-02-01 13:11:36 -0500

    This is done.

--- client/library/source/pages/join_page/join_page.tsx
+++ client/library/source/pages/join_page/join_page.tsx
@@ -378,8 +378,8 @@ const IMAGE_STYLE: React.CSSProperties = {
 };
 
 const DESKTOP_IMAGE_STYLE: React.CSSProperties = {
-  minWidth: '750px',
-  maxWidth: 'calc(100% - 521px - calc(100% - 1366px) / 2)',
+  minWidth: '744px',
+  maxWidth: 'calc(100% / 2 - 164px)',
   minHeight: '708px'
 };
 
