4f3c73d5f145f885c3e64a659d12d40e4ab6f035
Kamal <kamal@eidolonsystems.com>
2023-02-08 14:37:46 -0500

    Fixed build issue related to babel minify.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,46 +1,32 @@
-const path = require ('path');
-module.paths.push (path.resolve (process.cwd (), 'node_modules'));
-const MinifyPlugin = require ('babel-minify-webpack-plugin');
-const PROD = JSON.parse (process.env.PROD_ENV || '0');
-const minifyOpts = {};
-const minigyPluginOpts = {
-  test: /\.js($|\?)/i,
-};
-const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+module.paths.push(path.resolve(process.cwd(), 'node_modules'));
+const PROD = JSON.parse(process.env.PROD_ENV || '0');
 module.exports = {
-  devtool: PROD ? 'none' : 'source-map',
-  entry: path.resolve (process.cwd (), 'source/app.ts'),
+  devtool: PROD ? false : 'source-map',
+  entry: path.resolve(process.cwd(), 'source/app.ts'),
   mode: PROD ? 'production' : 'development',
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        loader: 'ts-loader',
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
       },
       {
         enforce: 'pre',
         test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  resolve: {
-    fallback: {
-      fs: false
-    }
+        loader: 'source-map-loader'
+      }
+    ]
   },
   output: {
-    path: path.resolve (process.cwd (), 'application'),
-    filename: 'server.js',
+    path: path.resolve(process.cwd(), 'application'),
+    filename: 'server.js'
   },
   performance: {
-    hints: false,
+    hints: false
   },
-  plugins: PROD ? [new MinifyPlugin (minifyOpts, minigyPluginOpts)] : [],
   resolve: {
     symlinks: false,
-    extensions: ['.ts', '.js', '.json'],
-  },
-  target: 'node',
-  externals: [nodeExternals()],
+    extensions: ['.ts', '.js', '.json']
+  }
 };
