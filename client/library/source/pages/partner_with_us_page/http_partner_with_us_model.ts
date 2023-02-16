fb58a8a5cf93aa50f34d9d578cf63845234b2fd9
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-02-16 18:34:22 -0500

    Added the http model. Removed the load method.

---
+++ client/library/source/pages/partner_with_us_page/http_partner_with_us_model.ts
@@ -0,0 +1,6 @@
+export class HttpPartnerWithUsModel extends PartnerWithUsModel {
+  public async sendEmail(name: string, email: string, profileLink: string,
+      message: string): Promise<void> {
+
+  }
+}
--- client/library/source/pages/partner_with_us_page/local_partner_with_us_model.ts
+++ client/library/source/pages/partner_with_us_page/local_partner_with_us_model.ts
@@ -1,10 +1,6 @@
 import { PartnerWithUsModel } from './partner_with_us_model';
 
 export class LocalPartnerWithUsModel extends PartnerWithUsModel {
-  public load(): void {
-    return;
-  }
-
   public async sendEmail(name: string, email: string, profileLink: string,
       message: string): Promise<void> {
     return;
