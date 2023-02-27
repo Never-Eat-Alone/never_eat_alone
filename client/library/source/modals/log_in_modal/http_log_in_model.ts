239bd076be6a17f899793edd49763cf5b6557645
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-02-27 16:11:42 -0500

    Added the google, facebook login and logout methods to httpLoginmodel.

--- client/library/source/modals/log_in_modal/http_log_in_model.ts
+++ client/library/source/modals/log_in_modal/http_log_in_model.ts
@@ -24,17 +24,55 @@ export class HttpLogInModel {
     return { user: User.makeGuest(), profileImage: UserProfileImage.NoImage() };
   }
 
-  public async googleLogIn(): Promise<{user: User, profileImage:
-      UserProfileImage}> {
-    return;
+  public async googleLogIn(email: string, token: string): Promise<{user: User,
+      profileImage: UserProfileImage}> {
+    const response = await fetch('/api/google_log_in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'email': email,
+        'token': token
+      })
+    });
+    if (response.status === 200) {
+      const responseObject = await response.json();
+      const user = User.fromJson(responseObject.user);
+      const profileImage = UserProfileImage.fromJson(
+        responseObject.profileImage);
+      return { user: user, profileImage: profileImage };
+    }
+    return { user: User.makeGuest(), profileImage: UserProfileImage.NoImage() };
   }
 
-  public async facebookLogIn(): Promise<{user: User, profileImage:
-      UserProfileImage}> {
-    return;
+  public async facebookLogIn(email: string, token: string): Promise<{user: User,
+      profileImage: UserProfileImage}> {
+    const response = await fetch('/api/facebook_log_in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'email': email,
+        'token': token
+      })
+    });
+    if (response.status === 200) {
+      const responseObject = await response.json();
+      const user = User.fromJson(responseObject.user);
+      const profileImage = UserProfileImage.fromJson(
+        responseObject.profileImage);
+      return { user: user, profileImage: profileImage };
+    }
+    return { user: User.makeGuest(), profileImage: UserProfileImage.NoImage() };
   }
 
   public async logOut(): Promise<boolean> {
-    return;
+    const response = await fetch('/api/log_out');
+    if (response.status === 200) {
+      return true;
+    }
+    return false;
   }
 }
