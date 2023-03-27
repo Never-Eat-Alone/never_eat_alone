a5f280de8183e6545faf3c985d372bff979264ea
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-03-27 17:55:18 -0400

    Removed the unused method from the database.

--- server/source/routes/dining_event.ts
+++ server/source/routes/dining_event.ts
@@ -13,7 +13,6 @@ export class DiningEventRoutes {
     /** Route to get the dining event card summaries on homepage. */
     app.get('/api/home_page/dining_event_card_summaries',
       this.getHomePageDiningEventCardSummaries);
-    app.get('/api/dining_events/:eventId', this.getDiningEventById);
     this.diningEventDatabase = diningEventDatabase;
   }
 
@@ -32,25 +31,6 @@ export class DiningEventRoutes {
     response.status(200).json({
       diningEventCardSummaryList: arrayToJson(diningEventCardSummaryList)
     });
-  }
-
-  private getDiningEventById = async (request, response) => {
-    const eventId = parseInt(request.params.eventId);
-    let diningEvent: DiningEvent;
-    try {
-      // Add the code to fetch the dining event by its ID
-      // e.g., diningEvent = await this.diningEventService.getDiningEventById(
-      // eventId);
-      // Replace the above comment with your actual implementation
-    } catch (error) {
-      response.status(400).json({
-        error: 'Bad Request: Please provide valid data.'
-      });
-      return;
-    }
-
-    // Send the response with the retrieved dining event, if any
-    response.status(200).json({ diningEvent: diningEvent.toJson() });
   }
 
   private diningEventDatabase: DiningEventDatabase;
