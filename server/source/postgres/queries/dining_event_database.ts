7a2092f21fd76f31787bd06c4f9a6f8daf26465e
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-03-28 16:16:54 -0400

    Finished adding the route related to loading the user upcoming events.

--- server/source/postgres/queries/dining_event_database.ts
+++ server/source/postgres/queries/dining_event_database.ts
@@ -16,7 +16,7 @@ export class DiningEventDatabase {
       de.start_at, de.end_at, de.cover_image_src, re.name AS re_name, \
       re.price_range AS re_price_range, de.total_capacity, de.color_code AS \
       de_color_code FROM dining_events AS de JOIN restaurants AS re ON \
-      de.restaurant_id = re.id order by start_at DESC limit $1', [7]);
+      de.restaurant_id = re.id ORDER BY start_at DESC LIMIT $1', [7]);
     if (!result || result.rows.length === 0) {
       return [];
     }
@@ -24,7 +24,7 @@ export class DiningEventDatabase {
     for (const row of result.rows) {
       let tempResult = await this.pool.query('SELECT cu.* FROM cuisines AS cu \
         JOIN restaurant_cuisines AS re ON re.cuisine_id = cu.id WHERE \
-        re.restaurant_id = $1 limit 10', [row.re_id]);
+        re.restaurant_id = $1 LIMIT 10', [row.re_id]);
       const cuisines = tempResult.rows.map(row => {
         return new Cuisine(parseInt(row.id), row.label, row.color_code);
       });
@@ -35,7 +35,7 @@ export class DiningEventDatabase {
         AND status = 'GOING'", [row.de_id]);
       if (tempResult.rows && tempResult.rows.length !== 0) {
         numberOfAttendees = parseInt(tempResult.rows[0].total);
-        numberOfSeatsAvailable -= parseInt(tempResult.rows[0].total);
+        numberOfSeatsAvailable -= numberOfAttendees;
       }
       let isAttending: boolean = false;
       tempResult = await this.pool.query("SELECT * FROM attendees WHERE \
@@ -49,6 +49,45 @@ export class DiningEventDatabase {
         row.re_name, PriceRange[row.re_price_range as keyof typeof PriceRange],
         cuisines, row.cover_image_src, numberOfAttendees,
         numberOfSeatsAvailable, isAttending, row.de_color_code);
+      events.push(event);
+    }
+    return events;
+  }
+
+  public loadUserFutureDiningEventCardSummaries = async (
+      userId: number): Promise<EventCardSummary[]> => {
+    const result = await this.pool.query(
+      "SELECT att.event_id, de.title, de.start_at, de.end_at, \
+      de.restaurant_id, de.cover_image_src, de.total_capacity, de.color_code, \
+      re.name, re.price_range FROM attendees AS att JOIN dining_events AS de \
+      ON att.event_id = de.id JOIN restaurants AS re ON \
+      re.id = de.restaurant_id WHERE att.user_id = $1 AND att.status = 'GOING' \
+      AND de.start_at >= NOW() ORDER BY de.start_at ASC", [userId]);
+    if (!result || result.rows.length === 0) {
+      return [];
+    }
+    const events: EventCardSummary[] = [];
+    for (const row of result.rows) {
+      let tempResult = await this.pool.query('SELECT cu.* FROM cuisines AS cu \
+        JOIN restaurant_cuisines AS re ON re.cuisine_id = cu.id WHERE \
+        re.restaurant_id = $1 LIMIT 10', [row.restaurant_id]);
+      const cuisines = tempResult.rows.map(row => {
+        return new Cuisine(parseInt(row.id), row.label, row.color_code);
+      });
+      let numberOfAttendees = 0;
+      let numberOfSeatsAvailable = parseInt(row.total_capacity);
+      tempResult = await this.pool.query(
+        "SELECT COUNT (event_id) AS total FROM attendees WHERE event_id = $1 \
+        AND status = 'GOING'", [row.event_id]);
+      if (tempResult.rows && tempResult.rows.length !== 0) {
+        numberOfAttendees = parseInt(tempResult.rows[0].total);
+        numberOfSeatsAvailable -= numberOfAttendees;
+      }
+      const event = new EventCardSummary(parseInt(row.event_id), row.title,
+        new Date(Date.parse(row.start_at)), new Date(Date.parse(row.end_at)),
+        row.name, PriceRange[row.price_range as keyof typeof PriceRange],
+        cuisines, row.cover_image_src, numberOfAttendees,
+        numberOfSeatsAvailable, true, row.color_code);
       events.push(event);
     }
     return events;
--- server/source/routes/dining_event.ts
+++ server/source/routes/dining_event.ts
@@ -1,4 +1,4 @@
-import { arrayToJson, DiningEvent, EventCardSummary
+import { arrayToJson, EventCardSummary
 } from '../../../client/library/source/definitions';
 import { DiningEventDatabase } from '../postgres/queries';
 
@@ -13,6 +13,8 @@ export class DiningEventRoutes {
     /** Route to get the dining event card summaries on homepage. */
     app.get('/api/home_page/event_list/:userId',
       this.getHomePageDiningEventCardSummaries);
+    app.get('/api/home_page/user_future_events/:userId',
+      this.getUserFutureDiningEventCardSummaries);
     this.diningEventDatabase = diningEventDatabase;
   }
 
@@ -32,6 +34,26 @@ export class DiningEventRoutes {
     }
     response.status(200).json({
       diningEventCardSummaryList: arrayToJson(diningEventCardSummaryList)
+    });
+  }
+
+  private getUserFutureDiningEventCardSummaries = async (request, response) => {
+    const userId = parseInt(request.params.userId);
+    let userFutureEventCardSummaryList: EventCardSummary[];
+    try {
+      userFutureEventCardSummaryList =
+        await this.diningEventDatabase.loadUserFutureDiningEventCardSummaries(
+          userId);
+    } catch (error) {
+      response.status(400).json({
+        userFutureEventCardSummaryList: [],
+        message: 'DATABASE_ERROR'
+      });
+      return;
+    }
+    response.status(200).json({
+      userFutureEventCardSummaryList: arrayToJson(
+        userFutureEventCardSummaryList)
     });
   }
 
