5c1cc03e24be053086399a51a74cd558b0d2306b
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-05-02 14:34:24 -0400

    Updated the config path as there was an error on the server side.

--- server/source/app.ts
+++ server/source/app.ts
@@ -4,7 +4,7 @@ import * as Express from 'express';
 import * as Session from 'express-session';
 import * as fs from 'fs';
 import Helmet from 'helmet';
-import * as Path from 'path';
+import * as path from 'path';
 import { Pool } from 'pg';
 import * as SGMail from '@sendgrid/mail';
 import { v4 as uuidv4 } from 'uuid';
@@ -32,7 +32,7 @@ const pgSession = require('connect-pg-simple')(Session);
 const initializePostgres = async (pool, dir, label, tableNames = []) => {
   if (tableNames.length === 0) {
     tableNames = fs.readdirSync(dir).filter(
-      f => Path.extname(f).toLowerCase() === '.sql').map(f => f.replace('.sql',
+      f => path.extname(f).toLowerCase() === '.sql').map(f => f.replace('.sql',
       ''));
   }
 
@@ -144,7 +144,7 @@ function runExpress(pool: Pool, config: any) {
   const attendeeDatabase = new AttendeeDatabase(pool);
   const attendeeRoutes = new AttendeeRoutes(app, attendeeDatabase);
   app.get('*', (request, response, next) => {
-    response.sendFile(Path.join(process.cwd(), 'public', 'index.html'));
+    response.sendFile(path.join(process.cwd(), 'public', 'index.html'));
   });
   app.listen(config.port, async () => {
     await initializePostgres(pool, '../source/postgres/types/', 'type');
@@ -171,7 +171,8 @@ function runExpress(pool: Pool, config: any) {
 }
 
 async function main() {
-  const config = JSON.parse(fs.readFileSync('config.json').toString());
+  const configPath = path.join(__dirname, 'config.json');
+  const config = JSON.parse(fs.readFileSync(configPath).toString());
   SGMail.setApiKey(config.send_grid_api_key);
   const pool = new Pool(config.database);
   runExpress(pool, config);
