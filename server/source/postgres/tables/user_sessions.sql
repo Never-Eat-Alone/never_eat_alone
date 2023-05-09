1d5d0c19d0bac6c3ba83ce4468a30de636caec31
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-05-09 16:35:26 -0400

    Removed the not null condition.

--- server/source/postgres/tables/user_sessions.sql
+++ server/source/postgres/tables/user_sessions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_sessions (
     sid         VARCHAR       NOT NULL COLLATE "default",
-    user_id     INTEGER       NOT NULL,
+    user_id     INTEGER       ,
     sess        JSON          NOT NULL,
     expire      TIMESTAMP(6)  NOT NULL
 )
@@ -17,8 +17,5 @@ ALTER TABLE user_sessions
         REFERENCES users (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE;
-
-ALTER TABLE user_sessions
-  ADD CONSTRAINT uq_user_sessions_user_id_sid UNIQUE (user_id, sid);
 
 CREATE INDEX IDX_session_expire ON user_sessions ("expire");
