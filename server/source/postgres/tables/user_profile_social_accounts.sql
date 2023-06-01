ab86f3a8def30769c82a91fb786bf02236f6f833
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-06-01 15:57:31 -0400

    Updated inserted example values in the database.

--- server/source/postgres/tables/user_profile_social_accounts.sql
+++ server/source/postgres/tables/user_profile_social_accounts.sql
@@ -11,7 +11,24 @@ INSERT INTO user_profile_images (
   platform,
   link
 ) VALUES
-  (1, 'FACEBOOK'),
-  (1, 'GOOGLE'),
-  (1, 'INSTAGRAM'),
-  (1, 'TWITTER');
+  (1, 'FACEBOOK', 'https://www.facebook.com/google'),
+  (2, 'INSTAGRAM', 'https://www.instagram.com/google'),
+  (3, 'INSTAGRAM', 'https://www.instagram.com/apple'),
+  (4, 'TWITTER', 'https://twitter.com/microsoft'),
+  (5, 'FACEBOOK', 'https://www.facebook.com/amazon'),
+  (6, 'INSTAGRAM', 'https://instagram.com/+Amazon'),
+  (7, 'INSTAGRAM', 'https://www.instagram.com/microsoft'),
+  (8, 'TWITTER', 'https://twitter.com/apple'),
+  (9, 'FACEBOOK', 'https://www.facebook.com/microsoft'),
+  (10, 'INSTAGRAM', 'https://instagram.com/microsoft'),
+  (11, 'INSTAGRAM', 'https://www.instagram.com/airbnb'),
+  (12, 'TWITTER', 'https://twitter.com/amazon'),
+  (13, 'FACEBOOK', 'https://www.facebook.com/apple'),
+  (14, 'FACEBOOK', 'https://facebook.com/apple'),
+  (15, 'INSTAGRAM', 'https://www.instagram.com/amazon'),
+  (16, 'TWITTER', 'https://twitter.com/google'),
+  (17, 'FACEBOOK', 'https://www.facebook.com/twitter'),
+  (18, 'TWITTER', 'https://twitter.com/twitter'),
+  (19, 'INSTAGRAM', 'https://www.instagram.com/twitter'),
+  (20, 'TWITTER', 'https://twitter.com/instagram'),
+  (21, 'FACEBOOK', 'https://www.facebook.com/instagram');
