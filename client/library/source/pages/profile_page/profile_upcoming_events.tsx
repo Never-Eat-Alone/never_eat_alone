a4fe6df4815b75f15a791b82961b51a8ca9ff6b5
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-07-10 13:53:59 -0400

    Adding the no events design to profile page.

--- client/library/source/pages/profile_page/profile_upcoming_events.tsx
+++ client/library/source/pages/profile_page/profile_upcoming_events.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as tinyColor from 'tinycolor2';
-import { DiningEventCard, ShowAllButton, ShowLessButton
-} from '../../components';
+import { DiningEventCard, ShowAllButton, ShowLessButton } from
+  '../../components';
 import { DisplayMode, EventCardSummary } from '../../definitions';
 
 interface Properties {
@@ -28,6 +28,27 @@ export class ProfileUpcomingEvents extends React.Component<Properties, State> {
   }
 
   public render(): JSX.Element {
+    if (this.props.eventList?.length === 0) {
+      return (
+        <div style={CONTAINER_STYLE} >
+          <h1 style={TITLE_STYLE} >
+            Upcoming Events (0)
+          </h1>
+          <div style={NO_EVENT_CONTAINER_STYLE} >
+            <div style={NO_EVENT_IMAGE_CONTAINER_STYLE} >
+              <img
+                style={NO_EVENT_IMAGE_STYLE}
+                src='resources/icons/no_events.svg'
+                alt='No Events Icon'
+              />
+            </div>
+            <p style={NO_EVENT_BOLD_TEXT_STYLE} >Nothing here yet</p>
+            <p style={NO_EVENT_DESCRIPTION_STYLE} >
+              You haven’t attended any events yet, why not check out some?
+            </p>
+          </div>
+        </div>);
+    }
     const cards = [];
     const numberOfDisplayedCards = Math.min(
       this.state.lastDisplayedCardIndex + 1, this.props.eventList.length);
@@ -107,6 +128,33 @@ const TITLE_STYLE: React.CSSProperties = {
   color: '#000000',
   padding: '0px',
   margin: '0px 0px 40px 0px'
+};
+
+const NO_EVENT_CONTAINER_STYLE: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  gap: '10px',
+  width: '100%'
+};
+
+const NO_EVENT_IMAGE_CONTAINER_STYLE: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '200px',
+  height: '150px',
+  backgroundColor: 'transparent',
+  marginTop: '30px'
+};
+
+const NO_EVENT_IMAGE_STYLE: React.CSSProperties = {
+  width: '100%',
+  height: '100%',
+  minWidth: '200px',
+  objectFit: 'cover'
 };
 
 const EVENT_CARDS_CONTAINER_STYLE: React.CSSProperties = {
