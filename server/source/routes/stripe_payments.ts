feb9d8dd931417633e4d97e281f7416d9a1ed23b
Shahrzad Masoumnia <shahrzad@nevereatalone.net>
2023-11-27 17:06:52 -0500

    Cleaned up the stripe route.

--- server/source/routes/stripe_payments.ts
+++ server/source/routes/stripe_payments.ts
@@ -3,22 +3,7 @@ export class StripePaymentRoutes {
   constructor(app: any, stripe: any) {
     app.post('/api/create-setup-intent', this.createSetupIntent);
     app.post('/api/create-payment-intent', this.createPaymentIntent);
-    app.post('/create-checkout-session', async (req, res) => {
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: 'https://nevereatalone.net',
-        cancel_url: 'https://nevereatalone.net',
-        automatic_tax: {enabled: true},
-      });
-
-      res.redirect(303, session.url);
-    });
+    app.post('/create-checkout-session', this.createCheckoutSession);
 
     this.stripe = stripe;    
   }
@@ -47,6 +32,23 @@ export class StripePaymentRoutes {
         error);
       response.status(400).send(error.message);
     }
+  }
+
+  private createCheckoutSession = async (request, response) => {
+    const session = await this.stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://nevereatalone.net',
+      cancel_url: 'https://nevereatalone.net',
+      automatic_tax: {enabled: true},
+    });
+
+    response.redirect(303, session.url);
   }
 
   /** The stripe payment api. */
